c6cca6b3f5c4a98547d67c08e99f7245873101f0
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2024-12-04 22:22:50 +0100

    refactor: dfs class refactoring

--- algorithms/dfs.py
+++ algorithms/dfs.py
@@ -1,27 +1,8 @@
-import heapq
-import itertools
+from utils import is_win_position, get_neighbors
 
 class RushHourSolverDFS:
         def __init__(self, board):
             self.initial_state = board
-
-        def is_goal(self, state):
-            for car_name, car in state.cars.items():
-                if car_name == "A":  # Цільова машина
-                    for x, y in car.positions:
-                        if y == state.grid_size - 1:  # Досягла виходу
-                            return True
-            return False
-
-        def get_neighbors(self, state):
-            neighbors = []
-            for car_name, car in state.cars.items():
-                for direction in ["up", "down", "left", "right"]:
-                    if state.is_valid_move(car, direction):
-                        new_state = state.copy()
-                        new_state.move_car(new_state.cars[car_name], direction)
-                        neighbors.append((new_state, (car_name, direction)))  # Зберігаємо пару
-            return neighbors
 
         def solve(self):
             stack = [(self.initial_state, [])]
@@ -35,12 +16,12 @@ class RushHourSolverDFS:
                     continue
                 visited.add(state_key)
 
-                if self.is_goal(current):
+                if is_win_position(current):
                     print("Solution found!")
                     print(f"Solution found in {len(path)} steps.")
                     return path
 
-                for neighbor, action in self.get_neighbors(current):
+                for neighbor, action in get_neighbors(current):
                     stack.append((neighbor, path + [action]))
 
             print("No solution found!")
